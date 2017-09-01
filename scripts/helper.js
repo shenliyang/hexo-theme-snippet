53c677b97ecd4d7250ab2a433bc688d1eae29071
shenliyang <tsl-shenliyang@tasly.com>
2017-09-01 16:11:37 +0800

    hexo init

---
+++ scripts/helper.js
@@ -0,0 +1,13 @@
+function pickImgs(post) {
+    var content = post.content.toString();
+    var imgRe = /<img.*?\ssrc=[\'\"]\S+[\'\"]\s.*?>/gim;
+    var urlRe = /(http:|https:|\/|\.)\S+(?="\s)/i;
+    var imgUrlsArr = content.match(imgRe);
+    var data = [];
+    imgUrlsArr&&imgUrlsArr.forEach(function (item) {
+       data.push(item.match(urlRe)[0]);
+    });
+    return data;
+}
+
+hexo.extend.helper.register('pickImgs',pickImgs);
