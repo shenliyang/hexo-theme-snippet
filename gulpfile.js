a8199bfdc47f885cdc6a45d24e744326e15a7328
shenliyang <tsl-shenliyang@tasly.com>
2017-09-07 17:46:18 +0800

    modify gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,20 @@
+(function() {
+    'use strict';
+    var gulp = require('gulp'),
+        less = require('gulp-less'),
+        minifycss = require('gulp-minify-css');
 
-'use strict';
-var gulp = require('gulp'),
-    less = require('gulp-less'),
-    minifycss = require('gulp-minify-css');
 
+    // CSS预处理
+    gulp.task('less-task', function() {
+        return gulp.src('./source/css/less/*.less')
+        .pipe(less())
+        .pipe(minifycss())
+        .pipe(gulp.dest('./source/css/'))
+    });
 
-// CSS预处理
-gulp.task('less-task', function() {
-    return gulp.src('./source/css/less/style.less')
-    .pipe(less())
-    .pipe(minifycss())
-    .pipe(gulp.dest('./source/css/'))
-});
+    // 默认执行的任务
+    gulp.task('default',['less-task'])
 
-// 默认执行的任务
-gulp.task('default',['less-task'])
+})();
 
