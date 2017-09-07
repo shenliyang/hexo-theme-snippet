bc84ea46cd164ddf72495334c2de0259a77aecf9
shenliyang <tsl-shenliyang@tasly.com>
2017-09-07 16:13:18 +0800

    增加持续集成构建配置文件

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+
+'use strict';
+let gulp = require('gulp'),
+    less = require('gulp-less');
+
+// CSS预处理
+gulp.task('less-task', function() {
+    return gulp.src('./source/css/less/style.less')
+    .pipe(less())
+    .pipe(gulp.dest('./source/css'))
+});
+
+// 默认执行的任务
+gulp.task('default',['less-task'],function(){
+  
+})
+
