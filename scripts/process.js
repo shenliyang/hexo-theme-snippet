bbf37584833856a7f642c3c4e044eee59e5cc3e1
shenliyang <tsl-shenliyang@tasly.com>
2018-01-09 11:19:13 +0800

    Add a link to the thumbnail article.

--- scripts/process.js
+++ scripts/process.js
@@ -1,5 +1,5 @@
 
-hexo.on('generateAfter', function () {
+hexo.on('ready', function () {
   hexo.log.info("=========================================");
   hexo.log.info("  Welcome to use Snippet theme for hexo  ");
   hexo.log.info("=========================================");
