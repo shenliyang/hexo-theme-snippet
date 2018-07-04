ca60289f2d66b2c3875f425e8ca22e8f0c704a98
dinphy <dinphy1205@126.com>
2018-07-05 07:52:39 +0800

    调整搜索到导航栏并适配移动端

--- source/js/app.js
+++ source/js/app.js
@@ -6,6 +6,8 @@ window.onload = function() {
         $fixnav = document.getElementById("fixnav"),
         $mnav = document.getElementById("mnav"), //获取导航三角图标
         $mainMenu = document.getElementById("main-menu"), //手机导航
+        $msearch = document.getElementById("msearch"), //获取搜索圆角图标
+        $mainSearch = document.getElementById("main-search"), //手机搜索	
         $process = document.getElementById('process'), //进度条
         $ajaxImgs = document.querySelectorAll('.img-ajax'), //图片懒加载
         $commentsCounter = document.getElementById('comments-count'),
@@ -35,6 +37,16 @@ window.onload = function() {
             $mainMenu.setAttribute("class","collapse navbar-collapse in");
         }
     };
+    
+    //手机搜索
+    $msearch.onclick = function(){
+        var searchOpen = $mainSearch.getAttribute("class");
+        if(searchOpen.indexOf("in") != '-1'){
+            $mainSearch.setAttribute("class","widget collapse");
+        } else {
+            $mainSearch.setAttribute("class","widget collapse in");
+        }
+    };    
 
     //首页文章图片懒加载
     function imgsAjax($targetEles) {
