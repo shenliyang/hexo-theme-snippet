dfc99a4f606069f7aa51a69effea4368a0e23efd
shenliyang <snippet@91h5.cc>
2018-07-23 17:47:21 +0800

    Update travis config

--- _travis.sh
+++ _travis.sh
@@ -30,7 +30,7 @@ function default(){
   mv .deploy_git/.git/ ./public/
   cd ./public
 
-cat <<EOF >> README.md 
+cat <<EOF >> README.md
 部署状态 | 集成结果 | 参考值
 ---|---|---
 完成时间 | $time | yyyy-mm-dd hh:mm:ss
@@ -40,26 +40,36 @@ cat <<EOF >> README.md
 仓库地址 | $TRAVIS_REPO_SLUG | owner_name/repo_name
 提交分支 | $TRAVIS_COMMIT | hash 16位
 提交信息 | $TRAVIS_COMMIT_MESSAGE |
-Job ID   | $TRAVIS_JOB_ID | 
-Job NUM  | $TRAVIS_JOB_NUMBER | 
+Job ID   | $TRAVIS_JOB_ID |
+Job NUM  | $TRAVIS_JOB_NUMBER |
 EOF
 
   git init
   git config user.name "shenliyang"
   git config user.email ""
   git add .
-  git commit -m "Build by Travis CI"
+  git commit -m "Update Blog By TravisCI With Build $TRAVIS_BUILD_NUMBER"
+  # Github Pages
   git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:master
-} 
+  # Gitee Pages
+  git push --force --quiet "https://shenliyang:${GITEE_TOKEN}@${GITEE_REF}" master:master
 
-case $1 in 
+  # Create Tag
+  git tag v1.2.$TRAVIS_BUILD_NUMBER -a -m "Auto Taged By TravisCI With Build $TRAVIS_BUILD_NUMBER"
+  # Github Pages
+  - git push --quiet "https://${GH_TOKEN}@${GH_REF}" master:master --tags
+  # Gitee Pages
+  - git push --quiet "https://shenliyang:${GITEE_TOKEN}@${GITEE_REF}" master:master --tags
+}
+
+case $1 in
     "success")
 	     success
        ;;
     "failure")
 	     failure
 	     ;;
-	         *) 
-       default	
+	         *)
+       default
 esac
 
